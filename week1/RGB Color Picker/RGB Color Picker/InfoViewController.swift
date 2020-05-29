a2a8684f313f9428cbbbbf507fb244cae3a9a9da
Donald Seo <deokjae.seo@gmail.com>
2020-05-29 14:18:38 -0400

    added InfoViewController, with UI outlets for that view

---
+++ week1/RGB Color Picker/RGB Color Picker/InfoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  InfoViewController.swift
+//  RGB Color Picker
+//
+//  Created by Donald Seo on 2020-05-29.
+//  Copyright © 2020 Donald Seo. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class InfoViewController: UIViewController {
+
+    @IBOutlet weak var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func closeButtonPressed(_ sender: UIButton) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
