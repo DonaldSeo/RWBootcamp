e41a9c689cf08067c56891535899abeec5646764
Donald Seo <deokjae.seo@gmail.com>
2020-05-28 22:57:37 -0400

    created initial xcode project

---
+++ week1/RGB Color Picker/RGB Color Picker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RGB Color Picker
+//
+//  Created by Donald Seo on 2020-05-28.
+//  Copyright © 2020 Donald Seo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
