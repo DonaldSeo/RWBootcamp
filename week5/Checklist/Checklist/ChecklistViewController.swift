f537f5de2d65e3dbd8e4ba321db59d1595c9d06b
Donald Seo <deokjae.seo@gmail.com>
2020-06-23 22:03:55 -0400

    initial commit of week5 course, finished part1-7

---
+++ week5/Checklist/Checklist/ChecklistViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Checklist
+//
+//  Created by Donald Seo on 2020-06-23.
+//  Copyright © 2020 Donald Seo. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1000
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        
+        if let label = cell.viewWithTag(1000) as? UILabel {
+            
+            switch indexPath.row % 5 {
+            case 0:
+                label.text = "Take a jog"
+            case 1:
+                label.text = "Watch a Movie"
+            case 2:
+                label.text = "Code an app"
+            case 3:
+                label.text = "Walk the dog"
+            case 4:
+                label.text = "Study design"
+            default:
+                break
+            }
+//            if indexPath.row == 0 {
+//                label.text = "Run a marathon"
+//            } else {
+//                label.text = "Sleep"
+//            }
+        }
+        
+        return cell
+    }
+
+
+}
+
