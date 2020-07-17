a7877c968e3a47a46feb23b8de430053c986af30
Donald Seo <deokjae.seo@gmail.com>
2020-07-17 17:38:27 -0400

    week8 core data stuff

--- week7/Bulleseye/Bulleseye/ContentView.swift
+++ week7/Bulleseye/Bulleseye/ContentView.swift
@@ -9,13 +9,59 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+  
+  @State var alertIsVisible: Bool = false
+  @State var sliderValue: Double = 50.0
+  
+  var body: some View {
+    VStack {
+      //target row
+      Spacer()
+      HStack {
+        Text("Put the bullseye as close as you can to: ")
+        Text("100")
+      }
+      Spacer()
+      //slider row
+      HStack {
+        Text("1")
+        Slider(value: self.$sliderValue, in: 1...100)
+        Text("100")
+      }
+      Spacer()
+      //button row
+      Button(action: {
+        print("Button pressed")
+        self.alertIsVisible = true
+      }) {
+        Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
+      }
+      .alert(isPresented: $alertIsVisible) { () -> Alert in
+        return Alert(title: Text("Hello there"), message: Text("The slider's value is \(Int(self.sliderValue.rounded(.towardZero)))"), dismissButton: .default(Text("Awesome")))
+      }
+      Spacer()
+      HStack {
+        Button(action: {}) {
+          Text("Start over")
+        }
+        Spacer()
+        Text("Score: ")
+        Text("999999")
+        Spacer()
+        Text("Round: ")
+        Text("999")
+        Spacer()
+        Button(action: {}) {
+          Text("Info")
+        }
+      }.padding(.bottom, 20)
+      
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+      ContentView().previewLayout(.fixed(width: 896, height: 414))
     }
 }
